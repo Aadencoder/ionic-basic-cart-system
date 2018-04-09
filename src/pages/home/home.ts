3e16ad2fadce2cb2212568c7f92a75828642d60d
DESKTOP-HEA3A3E\Aaden Sah <contactaaden101@gmail.com>
2018-04-09 15:51:30 +0545

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  items  : Object[] = [];
+  itemsInCart: Object[] = [];
+
+  constructor(public navCtrl: NavController) {
+
+    this.items  = [
+      { 'name': 'Hello World', quantityInCart: 0 , id: 1},
+      { 'name': 'Hello1 World', quantityInCart: 0 , id: 2},
+      { 'name': 'Hello2 World', quantityInCart: 0 , id: 3},
+      { 'name': 'Hello3 World', quantityInCart: 0 , id: 4}
+    ];
+  }
+
+  addToCart(item) {
+    item.quantityInCart += 1;
+    this.itemsInCart.push(item);  
+    console.log(this.itemsInCart);
+
+    localStorage.setItem('cart', JSON.stringify(this.itemsInCart));
+  }
+
+
+  goToList() {
+    this.navCtrl.push(ListPage);
+  }
+
+}
