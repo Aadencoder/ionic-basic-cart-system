d4565a35b54c9a2671bb5e8dd77ded3702eccb13
DESKTOP-HEA3A3E\Aaden Sah <contactaaden101@gmail.com>
2018-04-10 16:51:33 +0545

    QTY ISsue Resolved

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,11 +23,47 @@ export class HomePage {
   
 
   addToCart(item) {
-    item.quantityInCart += 1;
-    this.itemsInCart.push(item);  
+    
+    item.quantityInCart += 1;        
+    // declarations
+    var flag = 0;
+    var len = this.itemsInCart.length;
+    var i = 0;
+
+    console.log(len);
+
+
+
+    for (i; i < len; i++) {
+      // if name matches
+      console.log(item.id)
+      if (item.id == this.itemsInCart[i]['id']) {
+        flag = 1;
+        break;
+      } else {
+        // do nothing
+      }
+    }
+    
+        if (flag == 0) {
+      // new entry push in array
+          this.itemsInCart.push(item);
+        }
     console.log(this.itemsInCart);
 
     localStorage.setItem('cart', JSON.stringify(this.itemsInCart));
+
+
+
+
+
+    // WORKING QTY ISSUE
+    // item.quantityInCart += 1;
+    // this.itemsInCart.push(item);
+    // console.log(this.itemsInCart);
+
+    // localStorage.setItem('cart', JSON.stringify(this.itemsInCart));
+
   }
 
 
